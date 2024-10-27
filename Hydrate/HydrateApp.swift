1530858a1bae18a9b5d8cf8775b858922e9a2696
maryambsq <maryamx399@gmail.com>
2024-10-27 17:56:20 +0300

    Initial Commit

---
+++ Hydrate/HydrateApp.swift
@@ -0,0 +1,17 @@
+//
+//  HydrateApp.swift
+//  Hydrate
+//
+//  Created by Maryam Amer Bin Siddique on 24/04/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct HydrateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HydrateTests/HydrateTests.swift
@@ -0,0 +1,17 @@
+//
+//  HydrateTests.swift
+//  HydrateTests
+//
+//  Created by Maryam Amer Bin Siddique on 24/04/1446 AH.
+//
+
+import Testing
+@testable import Hydrate
+
+struct HydrateTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
